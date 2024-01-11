c75a6eb665fb077146e557fd1af0d7a88f89d951
artem05111 <artem.05111@gmail.com>
2024-01-11 17:56:19 +0200

    update Form page

---
+++ src/js/02-form.js
@@ -0,0 +1,44 @@
+// const { mergeAlias } = require('vite');
+
+const feedbackForm = document.querySelector('.feedback-form');
+
+const localStorageKey = 'feedback-form-state';
+const emailInput = feedbackForm.elements.email;
+const messageInput = feedbackForm.elements.message;
+
+const savedData = localStorage.getItem(localStorageKey);
+
+if (savedData) {
+  try {
+    const parsedData = JSON.parse(savedData);
+    if (parsedData.email && parsedData.message) {
+      emailInput.value = parsedData.email;
+      messageInput.value = parsedData.message;
+    }
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+  }
+}
+
+feedbackForm.addEventListener('input', () => {
+  const feedback = {
+    email: emailInput.value.trim(),
+    message: messageInput.value.trim(),
+  };
+  localStorage.setItem(localStorageKey, JSON.stringify(feedback));
+});
+feedbackForm.addEventListener('submit', evt => {
+  evt.preventDefault();
+
+  if (emailInput.value.trim() === '' || messageInput.value.trim() === '') {
+    alert('Заповни всі поля');
+    return;
+  }
+  localStorage.removeItem(localStorageKey);
+  console.log({
+    email: emailInput.value.trim(),
+    message: messageInput.value.trim(),
+  });
+  feedbackForm.reset();
+});
